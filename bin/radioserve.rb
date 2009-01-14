206dcbf008d2c494485ef8ee207dd76b565769ec
doug <dougtko@gmail.com>
2009-01-14 02:58:00 -0500

    cleanup, some bug fixes

--- bin/radioserve.rb
+++ bin/radioserve.rb
@@ -6,8 +6,14 @@
 
 require 'lib/rham'
 require 'drb'
+require 'drb/acl'
 
-DRb.start_service nil, Rham.new
+acl = ACL.new(%w(deny all
+                 allow localhost
+                 allow igotinternets.net))
+DRb.install_acl(acl)
+
+DRb.start_service("druby://192.168.0.6:7176", Rham.new)
 puts DRb.uri
 
 DRb.thread.join
